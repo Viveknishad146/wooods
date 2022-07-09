8ef79ef7ded700e1acee488a79fc689832436559
Vivek Nishad <vivekmips.cs16@mpgi.edu.in>
2022-07-09 20:06:43 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--<link rel="stylesheet" href="style.css">-->
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!----------icon css--------------->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">    
+    <!--------------carousel indicators------------------------------->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar">
+    <ul>
+        <li>HOME</li>
+        <li>OURWORK</li>
+        <li>CONTACTS</li>
+    </ul>
+    <ul><li>ABOUT</li></ul>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+include 'header.php';
+?>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>Wooods</title>
+    </head>
+    <body>
+    <div class="banner" style="margin-top: 2%;">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="images/banner7.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/v1.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/bed.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+    <div>
+    <!------------------part 2----------------------------->
+    <div class="div1" style="text-align: center; font-style: Serif; margin-top: 10%;">
+        <center><h1>Decorate Your House</h1></center>
+    </div>
+    <div class="div2" style="color: grey; margin-top: 5%; text-align: center; font-style: Serif;">
+        <i><h6>Definitely you are at right place</h6></i>
+    </div>    
+    <div class="div3" style="margin-top: 2%; text-align: center; font-style: Serif;">
+        <i><h6>Here a wide range of furniture's design, you think, we do it. We have a number of options and number of designs, and number of categories of furnitures that we make. It's your wish , how you like to decorate your house.</h6></i>
+    </div>  
+    <!------------------part 3-------------------------------->
+    
+    <div class="container" style="margin: auto;">
+        <div class="row">
+            <div class="col-sm" style="margin: 2%; text-align:center;">
+                <img src="images/almirah.jpg" class="img-fluid rounded" alt="Responsive image">
+            </div>
+            <div class="col-sm" style="margin: 2%;">
+                <center><img src="images/sofa1.jpg" class="img-fluid rounded" alt="Responsive image"></center>
+            </div>
+            <div class="col-sm" style="margin: 2%;">
+                <center><img src="images/v1.jpg" class="img-fluid rounded" alt="Responsive image"></center>
+            </div>
+        </div>
+    </div>
+    <!-----------------part 4----------------------------------->
+    <div class="board" style="background-color: black; margin-top: 5%; height: 500px;">
+    <center><b><h1 style="color: white;">Our Work</h1></b></center>
+    <center><b><h6 style="color: grey;">Move next</h6></b></center>
+        <div class="container" style="margin-top: 10%;">
+            <div class="row">
+                <div class="col-sm" style="margin: 2%; text-align:center;">
+                    <img src="images/almirah.jpg" class="img-fluid" alt="Responsive image">
+                    <div style="background-color: white; height: 40%;"><b><h1 style="color: black; font-size: 2vw;">How's It</h1></b><h1 style="font-size: 2vw;">Everything You Like</h1></div>               
+                </div>
+                <div class="col-sm" style="margin: 2%; text-align:center;">
+                    <img src="images/sofa1.jpg" class="img-fluid" alt="Responsive image">
+                    <div style="background-color: white; height: 40%;"><b><h1 style="color: black; font-size: 2vw;">How's It</h1></b><h1 style="font-size: 2vw;">Everything You Like</h1></div>               
+                </div>
+                <div class="col-sm" style="margin: 2%; text-align:center;">
+                    <img src="images/v1.jpg" class="img-fluid" alt="Responsive image">
+                    <div style="background-color: white; height: 40%;"><b><h1 style="color: black; font-size: 2vw;">How's It</h1></b><h1  style="font-size: 2vw;">Everything You Like</h1></div>               
+                </div>
+            </div>
+        </div>
+    </div>
+    <!----------------part 5------------------------->
+    <div class="contact" style="background-color: white; height: 70%; margin:10%; text-align: center; color: black;">
+        <i><b><h1>Feel free to contact us</h1></b></i>
+        <i><h5>+91 9140231978</h5></i>
+    </div>
+    <!---------------part 6------------------------>
+    <div>
+        <img src="images/banner4.jpg" class="img-fluid" alt="Responsive image"></img>
+    </div>
+    <!--------------part 7------------------------------> 
+    </body>
+</html>
+<?php
+include "footer.html";
+?>
